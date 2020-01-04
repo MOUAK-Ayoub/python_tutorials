b0ebcec4604a1c0128bb4592dfa4e5c642e7112e
ayoub.mouak.2015@gmail.com <Bat2020billy1994>
2020-01-04 03:02:18 +0100

    Classification with SVM Clustering using K_means

--- Tutorial 1/beginner_tutorial/Clustering/Kmeans.py
+++ Tutorial 1/beginner_tutorial/Clustering/Kmeans.py
@@ -34,6 +34,6 @@ if  __name__ == '__main__':
     labels, centers = KmeansRandomData.KmeansClustering(Clus_dataSet, 3)
     data["Clus_km"] = labels
     print centers
-    print data.groupby('Clus_km').mean()
+    print data.groupby('Clus_kmm').mean()
     plot2D(X)
 
